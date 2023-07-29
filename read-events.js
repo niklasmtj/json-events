4b4cc93e1481b764329018179e88701c48ead749
Niklas Metje <22395665+niklasmtj@users.noreply.github.com>
2023-07-29 15:45:41 +0200

    feat: add ability to edit events
    Signed-off-by: Niklas Metje <22395665+niklasmtj@users.noreply.github.com>

--- read-events.js
+++ read-events.js
@@ -1,11 +1,33 @@
-const fs = require('fs');
-const event = require('./new-event.json');
-const ee = require('./events.json');
+const fs = require("fs");
+const eventPayload = require(process.env.GITHUB_EVENT_PATH ||
+  "./github/workflow/event.json");
+
+const event = require("./new-event.json");
+const ee = require("./events.json");
+
+event.id = eventPayload.issue.number;
 
 const isEmpty = (event) => {
-  return Object.keys(event).length === 0
+  return Object.keys(event).length === 0;
+};
+
+switch (eventPayload.action) {
+  case "opened":
+    !isEmpty(event) && ee.events.push(event);
+    console.log(event);
+    break;
+  case "edited":
+    if (isEmpty(event)) {
+      break;
+    }
+    const eventIndex = ee.events.findIndex((e) => e.id === event.id);
+    if (!eventIndex) {
+      ee.events.push(event);
+      break;
+    }
+    ee.events[eventIndex] = event;
+  default:
+    break;
 }
 
-!isEmpty(event) && ee.events.push(event)
-
-fs.writeFileSync('events.json', JSON.stringify(ee));
+fs.writeFileSync("events.json", JSON.stringify(ee));
